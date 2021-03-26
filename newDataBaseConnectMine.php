7de295c9db82aa1a49b95e68809376f8b5b8add7
Shirley <markshir@gmail.com>
2021-03-26 22:54:25 +0300

    the file that sets the connection

---
+++ newDataBaseConnectMine.php
@@ -0,0 +1,8 @@
+<?php
+  $servername = "intranet.fil.ion.ucl.ac.uk";
+  $port = 3306;
+  $username = "smark";
+  $password = "djh7ArGu3IWgKznH";
+  $dbname = "smark";
+ // $table = "mvaghi_newtable";
+?>
